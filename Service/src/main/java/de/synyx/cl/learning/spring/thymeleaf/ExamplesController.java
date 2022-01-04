4a3eb68b905e2e2d5239ca4f0364184a31b56b44
lange <lange@synyx.de>
2022-01-04 17:06:10 +0100

    Beispiel für restricted access Problem bei Thymeleaf (tritt hier aber nicht auf)

---
+++ Service/src/main/java/de/synyx/cl/learning/spring/thymeleaf/ExamplesController.java
@@ -0,0 +1,21 @@
+package de.synyx.cl.learning.spring.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ExamplesController {
+
+    @GetMapping("/examples")
+    public String index() {
+        return "examples/index";
+    }
+
+    @GetMapping("/examples/restrictedAccessThymeleafExample")
+    public String restrictedAccessThymeleafExample(@RequestParam(value="index", defaultValue = "0") String index,Model model) {
+        model.addAttribute("index", index);
+        return "examples/restrictedAccessThymeleafExample";
+    }
+}
